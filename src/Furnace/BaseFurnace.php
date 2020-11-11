2812d16810b4838a1eca4ad1661117646f4c9fa5
Alperen Sancak <49764317+ByAlperenS@users.noreply.github.com>
2020-11-11 22:29:40 +0300

    Add files via upload

---
+++ src/Furnace/BaseFurnace.php
@@ -0,0 +1,247 @@
+<?php
+
+
+/*
+
+Plugin Author: ByAlperenS
+
+Messenger: Alperen Sancak
+Facebook: Alperen Sancak
+Discord: ByAlperenS#5361
+
+*/
+
+
+namespace Furnace;
+
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\event\block\BlockBreakEvent as BBE;
+use pocketmine\event\block\BlockPlaceEvent as BPE;
+use pocketmine\nbt\tag\ByteArrayTag;
+use pocketmine\event\player\PlayerInteractEvent as PIE;
+use pocketmine\event\player\{PlayerJoinEvent as PJE, PlayerQuitEvent as PQE};
+use pocketmine\item\Item;
+use pocketmine\level\Position;
+use pocketmine\block\Block;
+use pocketmine\item\ItemFactory;
+use pocketmine\item\ItemIds;
+use pocketmine\tile\Tile;
+use pocketmine\tile\Furnace;
+use pocketmine\block\BurningFurnace;
+use pocketmine\level\Level;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\event\inventory\FurnaceBurnEvent as FBE;
+use pocketmine\event\inventory\FurnaceSmeltEvent as FSE;
+use pocketmine\inventory\FurnaceInventory;
+use muqsit\invmenu\InvMenuHandler;
+use pocketmine\inventory\transaction\action\SlotChangeAction;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\ContainerClosePacket;
+use Furnace\Menus\FurnaceUpgradeMenu;
+
+// Needs EconomyAPI to work
+
+use onebone\economyapi\EconomyAPI;
+
+class BaseFurnace extends PluginBase implements Listener
+{
+    
+    /** @var bool */
+	private $cancel_send = true;
+
+    public $title = C::GREEN . "FastFurnace > ";
+
+    private static $instance;
+	
+	public function onLoad(){
+		self::$instance = $this;
+	}
+	
+	public static function getInstance(): BaseFurnace{
+		return self::$instance;
+	}
+
+    public $config;
+
+    public function onEnable()
+    {
+        $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+        if (!$economy instanceof EconomyAPI) {
+            $this->getLogger()->critical("You need EconomyAPI (https://poggit.pmmp.io/p/EconomyAPI/)");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+        }
+        if(!InvMenuHandler::isRegistered()){
+            InvMenuHandler::register($this);
+        }
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("Plugin Enable - ByAlperenS");
+        @mkdir($this->getDataFolder());
+        $this->config = new Config($this->getDataFolder() . "Furnaces.yml", Config::YAML);
+    }
+
+    public function onBlockPlace(BPE $e)
+    {
+        $p = $e->getPlayer();
+        $block = $e->getBlock();
+        $coordinate = $block->getX() . ":" . $block->getY() . ":" . $block->getZ();
+        $this->config->set($coordinate, [
+            "Level" => 0,
+            "Owner" => $p->getName(),
+            "Coordinates" => $coordinate
+        ]);
+        $this->config->save();
+    }
+
+    public function onPlayerQuit(PQE $e)
+    {
+        $p = $e->getPlayer();
+
+        $this->config->save();
+    }
+
+    public function onBlockBreak(BBE $e)
+    {
+        $p = $e->getPlayer();
+        $block = $e->getBlock();
+        $itemhand = $p->getInventory()->getItemInHand();
+        $coordinate = $block->getX() . ":" . $block->getY() . ":" . $block->getZ();
+
+        if ($block->getId() == Block::FURNACE or $block->getId() == Block::BURNING_FURNACE) {
+            if ($itemhand->getId() == Item::WOODEN_PICKAXE) {
+                if ($p->getName() == $this->config->get($coordinate)["Owner"]) {
+                    $e->setCancelled();
+                    $menu = new FurnaceUpgradeMenu($coordinate);
+                    $menu->sendTo($p);
+                }else {
+                    $e->setCancelled();
+                    $p->sendMessage($this->title . C::GRAY . "You Are Not The Owner Of This Furnace !");
+                }
+            }else {
+                if ($p->getName() == $this->config->get($coordinate)["Owner"]) {
+                    $this->config->remove($coordinate);
+                    $this->config->save();
+                    $p->sendMessage($this->title . C::GRAY . "Furnace Level Have Been Reset !");
+                }else {
+                    if (!$p->isOp()) {
+                        $e->setCancelled();
+                        $p->sendMessage($this->title . C::GRAY . "You Are Not The Owner Of This Furnace !");
+                    }
+                }   
+            }
+        }
+    }
+
+    public function onDataPacketSend(DataPacketSendEvent $event) : void{
+		if($this->cancel_send && $event->getPacket() instanceof ContainerClosePacket){
+			$event->setCancelled();
+		}
+	}
+
+	/**
+	 * @param DataPacketReceiveEvent $event
+	 * @priority NORMAL
+	 * @ignoreCancelled true
+	 */
+	public function onDataPacketReceive(DataPacketReceiveEvent $event) : void{
+		if($event->getPacket() instanceof ContainerClosePacket){
+			$this->cancel_send = false;
+			$event->getPlayer()->sendDataPacket($event->getPacket(), false, true);
+			$this->cancel_send = true;
+		}
+	}
+
+    public function getConfig(): Config
+    {
+        return $this->config;
+    }
+
+    public function onFurnaceBurn(FBE $e)
+    {
+        $furnace = $e->getFurnace();
+        $block = $e->getBlock();
+        $coordinate = $block->getX() . ":" . $block->getY() . ":" . $block->getZ();
+        $owner = $this->config->get($coordinate)["Owner"];
+        $level = $this->config->get($coordinate)["Level"];
+        $newcoordinate = $this->config->get($coordinate)["Coordinates"];
+        $separate = explode(":", $newcoordinate);
+
+        if ($furnace instanceof Furnace) {
+            if ($separate[0] == $block->getX() and $separate[1] == $block->getY() and $separate[2] == $block->getZ()) { 
+                switch ($level) {
+                    case '0':
+                        // Don't Touch
+                        break;
+                    case '1':
+                        $e->setBurnTime(300);
+                        $e->setBurning(true);
+                        break;
+                    case '2':
+                        $e->setBurnTime(600);
+                        $e->setBurning(true);
+                        break;
+                    case '3':
+                        $e->setBurnTime(900);
+                        $e->setBurning(true);
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    public function onSmelt(FSE $e)
+    {
+        $block = $e->getBlock();
+        $furnace = $e->getFurnace();
+        $coordinate = $block->getX() . ":" . $block->getY() . ":" . $block->getZ();
+        $owner = $this->config->get($coordinate)["Owner"];
+        $level = $this->config->get($coordinate)["Level"];
+        $newcoordinate = $this->config->get($coordinate)["Coordinates"];
+        $separate = explode(":", $newcoordinate);
+
+        if ($furnace instanceof Furnace) {
+            if ($separate[0] == $block->getX() and $separate[1] == $block->getY() and $separate[2] == $block->getZ()) {
+                switch ($level) {
+                    case '0':
+                        // Don't Touch
+                        break;
+                    case '1':
+                        $itemid = $e->getResult()->getId();
+                        $itemmeta = $e->getResult()->getDamage();
+                        $itemcount = $e->getResult()->getCount() + 1;
+                        $newitem = Item::get($itemid, $itemmeta, $itemcount);
+                        $e->setResult($newitem);
+                        break;
+                    case '2':
+                        $itemid = $e->getResult()->getId();
+                        $itemmeta = $e->getResult()->getDamage();
+                        $itemcount = $e->getResult()->getCount() + 2;
+                        $newitem = Item::get($itemid, $itemmeta, $itemcount);
+                        $e->setResult($newitem);
+                        break;
+                    case '3':
+                        $itemid = $e->getResult()->getId();
+                        $itemmeta = $e->getResult()->getDamage();
+                        $itemcount = $e->getResult()->getCount() + 3;
+                        $newitem = Item::get($itemid, $itemmeta, $itemcount);
+                        $e->setResult($newitem);
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ src/Furnace/Menus/FurnaceUpgradeMenu.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Furnace\Menus;
+
+use muqsit\invmenu\InvMenu;
+use pocketmine\item\Item;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\inventory\transaction\action\SlotChangeAction;
+use onebone\economyapi\EconomyAPI;
+use pocketmine\utils\TextFormat as C;
+use Furnace\BaseFurnace;
+
+class FurnaceUpgradeMenu
+{
+
+    public $price = [
+        "Level-1" => 2500,
+        "Level-2" => 4500,
+        "Level-3" => 6500
+    ];
+
+    public $title = C::GREEN . "FastFurnace > ";
+
+    private $menu;
+
+    private $coordinate;
+
+    public function __construct($coordinate)
+    {
+        $config = BaseFurnace::getInstance()->getConfig();
+        $owner = $config->get($coordinate)["Owner"];
+        $level = $config->get($coordinate)["Level"];
+
+        $menu = new InvMenu(InvMenu::TYPE_CHEST);
+        $menu->setName("Furnace Settings");
+        $menu->readonly();
+        $menu->setListener([$this, "onClick"]);
+        for ($i=0; $i < 27; $i++) { 
+            $menu->getInventory()->setItem($i, Item::get(160,8,1)->setCustomName(""));
+        }
+        $menu->getInventory()->setItem(11, Item::get(61,0,1)->setCustomName(C::YELLOW . "Furnace Owner: " . C::GRAY . $owner));
+        $menu->getInventory()->setItem(13, Item::get(384,0,1)->setCustomName(C::YELLOW . "Furnace Level: " . C::GRAY . $level));
+
+        switch ($level) {
+            case '0':
+                $menu->getInventory()->setItem(15, Item::get(388,0,1)->setCustomName(C::YELLOW . "Furnace Level Upgrade")->setLore([C::YELLOW . "Price: " . C::GRAY . $this->price["Level-1"]]));
+                break;
+            case '1':
+                $menu->getInventory()->setItem(15, Item::get(388,0,1)->setCustomName(C::YELLOW . "Furnace Level Upgrade")->setLore([C::YELLOW . "Price: " . C::GRAY . $this->price["Level-2"]]));
+                break;
+            case '2':
+                $menu->getInventory()->setItem(15, Item::get(388,0,1)->setCustomName(C::YELLOW . "Furnace Level Upgrade")->setLore([C::YELLOW . "Price: " . C::GRAY . $this->price["Level-3"]]));
+                break;
+            
+            default:
+                $menu->getInventory()->setItem(15, Item::get(388,0,1)->setCustomName(C::YELLOW . "Furnace Is Already Maximum Level !"));
+                break;
+        }
+
+        $menu->getInventory()->setItem(22, Item::get(339,0,1)->setCustomName(C::RED . "If You Break The Furnace The Levels Will Reset !"));
+        $this->menu = $menu;
+        $this->coordinate = $coordinate;
+    }
+
+    public function onClick(Player $p, Item $itemClick, Item $itemClickWith, SlotChangeACtion $action): bool
+    {
+        $config = BaseFurnace::getInstance()->getConfig();
+        $owner = $config->get($this->coordinate)["Owner"];
+        $level = $config->get($this->coordinate)["Level"];
+
+        if ($itemClick->getId() == 388) {
+            switch ($level) {
+                case '0':
+                    if (EconomyAPI::getInstance()->myMoney($p) >= $this->price["Level-1"]) {
+                        EconomyAPI::getInstance()->reduceMoney($p, $this->price["Level-1"]);
+                        $new = str_replace($config->get($this->coordinate)["Level"], 1, $config->get($this->coordinate)["Level"]);
+                        $config->set($this->coordinate, [
+                            "Level" => $new,
+                            "Owner" => $p->getName(),
+                            "Coordinates" => $this->coordinate
+                        ]);
+                        $config->save();
+                        $p->removeWindow($action->getInventory());
+                        $p->sendMessage($this->title . C::GRAY . "The Furnace Level Has Been Raised Successfully !");
+                    }else {
+                        $p->sendMessage($this->title . C::GRAY . "You Have No Money !");
+                        $p->removeWindow($action->getInventory());
+                    }
+                    break;
+                case '1':
+                    if (EconomyAPI::getInstance()->myMoney($p) >= $this->price["Level-2"]) {
+                        EconomyAPI::getInstance()->reduceMoney($p, $this->price["Level-2"]);
+                        $new = str_replace($config->get($this->coordinate)["Level"], 2, $config->get($this->coordinate)["Level"]);
+                        $config->set($this->coordinate, [
+                            "Level" => $new,
+                            "Owner" => $p->getName(),
+                            "Coordinates" => $this->coordinate
+                        ]);
+                        $config->save();
+                        $p->removeWindow($action->getInventory());
+                        $p->sendMessage($this->title . C::GRAY . "The Furnace Level Has Been Raised Successfully !");
+                    }else {
+                        $p->sendMessage($this->title . C::GRAY . "You Have No Money !");
+                        $p->removeWindow($action->getInventory());
+                    }
+                    break;
+                case '2':
+                    if (EconomyAPI::getInstance()->myMoney($p) >= $this->price["Level-3"]) {
+                        EconomyAPI::getInstance()->reduceMoney($p, $this->price["Level-3"]);
+                        $new = str_replace($config->get($this->coordinate)["Level"], 3, $config->get($this->coordinate)["Level"]);
+                        $config->set($this->coordinate, [
+                            "Level" => $new,
+                            "Owner" => $p->getName(),
+                            "Coordinates" => $this->coordinate
+                        ]);
+                        $config->save();
+                        $p->removeWindow($action->getInventory());
+                        $p->sendMessage($this->title . C::GRAY . "The Furnace Level Has Been Raised Successfully !");
+                    }else {
+                        $p->sendMessage($this->title . C::GRAY . "You Have No Money !");
+                        $p->removeWindow($action->getInventory());
+                    }
+                    break;
+                
+                default:
+                    $p->removeWindow($action->getInventory());
+                    $p->sendMessage($this->title . C::GRAY . "Furnace Is Already Maximum Level !");
+                    break;
+            }
+        }
+        return true;
+    }
+
+    public function sendTo(Player $p): void
+    {
+        $this->menu->send($p);
+    }
+}
